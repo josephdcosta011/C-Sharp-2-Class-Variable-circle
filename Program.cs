862b27fc20074c05b8dcd96746b49e08fcd2834e
Joseph Albert D Costa <132787171+josephdcosta011@users.noreply.github.com>
2023-07-24 23:29:25 +0600

    Add files via upload
    🎉 Welcome to the InputfromUser Galaxy! 🌌💫 Step into the realm of interactive C# programming with this captivating code. 🖥️✨ Embrace the power of user input as you calculate the circumference with just a radius! 🔍🎯 Unleash your coding prowess as you embark on an exciting journey of exploration and discovery. 🚀🔭 Let's dive into the magic of C# and create mesmerizing experiences together! 🌟🚀

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace InputfromUser
+{
+
+    class Input
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            double circum;
+            double pie = 3.142;
+            //MAIN STARTS
+            Console.WriteLine("Input Radius:");
+            object on = Console.ReadLine();
+            num = Convert.ToInt32(on);
+            //MAIN ENDS
+            circum = num * pie;
+
+            Console.WriteLine("Circumference is {0}", circum);
+
+        }
+    }
+}
